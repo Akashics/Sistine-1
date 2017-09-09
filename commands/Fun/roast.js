1ec28798f71672c1babeb37e4e0b922b2e18ba37
Jordan Jones <jordan@vps145886.vps.ovh.ca>
2017-09-09 02:39:51 -0400

    needs msg huh

--- commands/Fun/roast.js
+++ commands/Fun/roast.js
@@ -6,7 +6,7 @@ module.exports = class extends Command {
 	constructor(...args) {
 		super(...args, {
 			name: 'roast',
-			enabled: false,
+			enabled: true,
 			runIn: ['text'],
 			cooldown: 0,
 			aliases: ['roastme'],
