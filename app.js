75d60b7d00a0ee42db0cde27609d00b008ffd329
Kashall <Kashalls@users.noreply.github.com>
2017-09-26 12:21:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const Client = require('./util/lib/Client');
 const { botToken } = require('./keys.json');
+const sistinePermissionLevels = new PermissionLevels()
+  .addLevel(0, false, () => true)
+  .addLevel(1, false, (client, msg) => msg.guild && msg.guild.settings.dj && msg.member.roles.has(msg.guild.settings.dj))
+  .addLevel(2, false, (client, msg) => msg.guild && msg.guild.settings.modRole && msg.member.roles.has(msg.guild.settings.modRole))
+  .addLevel(7, false, (client, msg) => msg.guild && msg.member === msg.guild.owner)
+  .addLevel(9, true, (client, msg) => msg.author === client.owner)
+  .addLevel(10, false, (client, msg) => msg.author === client.owner);
 
 const Sistine = new Client({
   clientOptions: {
