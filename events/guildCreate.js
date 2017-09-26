c18e6e8ae9afd4c9f9b1ac468df311a75852ec6a
Kashall <Kashalls@users.noreply.github.com>
2017-09-26 12:10:09 -0700

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -12,16 +12,16 @@ module.exports = class extends Event {
       await guild.leave();
     }
 
-    this.client.datadog.increment('client.guildJoin');
+    this.client.datadog.gauge('client.guildJoin');
 
     dBots(this.client.guilds.size);
     dBotsOrg(this.client.guilds.size);
 
-    this.client.user.setActivity(`s>help — Shard #${this.client.shard.id}`).catch((err) => {
+    this.client.user.setActivity(`sistine.ml | s>help ${this.client.guilds.size}`).catch((err) => {
       this.client.emit('log', err, 'error');
     });
 
-    this.client.channels.get('341768632545705986').send(`<:tickYes:315009125694177281> Joined ${guild.name} (${guild.id}) with ${guild.memberCount} members owned by ${guild.owner.user.tag}`);
+    this.client.channels.get('341768632545705986').send(`<:tickYes:315009125694177281> Joined \`"${guild.name}" (${guild.id})\` with ${guild.memberCount} members owned by \`${guild.owner.user.tag}\``);
   }
 
 };
