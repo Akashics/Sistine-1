ce80c03ba59c2e3cc440cf77de514dd38c757ae7
Kashall <Kashalls@users.noreply.github.com>
2017-09-26 12:20:34 -0700

    Update Client.js

--- util/lib/Client.js
+++ util/lib/Client.js
@@ -7,7 +7,7 @@ const bans = require('../../banlist.json');
 const dogstatsd = new StatsD();
 const Raven = require('raven');
 
-const permissionLevels = new PermissionLevels()
+const sistinePermissionLevels = new PermissionLevels()
   .addLevel(0, false, () => true)
   .addLevel(1, false, (client, msg) => msg.guild && msg.guild.settings.dj && msg.member.roles.has(msg.guild.settings.dj))
   .addLevel(2, false, (client, msg) => msg.guild && msg.guild.settings.modRole && msg.member.roles.has(msg.guild.settings.modRole))
@@ -15,10 +15,11 @@ const permissionLevels = new PermissionLevels()
   .addLevel(9, true, (client, msg) => msg.author === client.owner)
   .addLevel(10, false, (client, msg) => msg.author === client.owner);
 
+//0 is Default, 1 is a DJ Role, 2 is a Moderator Role, 7 is Guild Owner, 9 and 10 are Bot Owner
 class Sistine extends Client {
 
   constructor(options) {
-    super(Object.assign(options, { permissionLevels }));
+    super(Object.assign(options, { sistinePermissionLevels }));
     Object.defineProperty(this, 'keys', { value: keys });
     this.datadog = dogstatsd;
     this.queue = new Music();
