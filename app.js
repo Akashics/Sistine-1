093314c5eda272995fb28ec0ef5c62783ee020f3
Kashall <Kashalls@users.noreply.github.com>
2017-09-26 12:25:43 -0700

    Merged App.js with Client.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const Client = require('./util/lib/Client');
-const { botToken } = require('./keys.json');
+const { Client, PermissionLevels } = require('klasa');
+const Music = require('./util/lib/Music');
+const keys = require('keys.json');
+const bans = require('banlist.json');
+const StatsD = require('node-dogstatsd').StatsD;
+const dogstatsd = new StatsD();
+const raven = require('raven');
+
 const sistinePermissionLevels = new PermissionLevels()
   .addLevel(0, false, () => true)
   .addLevel(1, false, (client, msg) => msg.guild && msg.guild.settings.dj && msg.member.roles.has(msg.guild.settings.dj))
@@ -8,7 +14,20 @@ const sistinePermissionLevels = new PermissionLevels()
   .addLevel(9, true, (client, msg) => msg.author === client.owner)
   .addLevel(10, false, (client, msg) => msg.author === client.owner);
 
-const Sistine = new Client({
+class SistineClient extends Client {
+
+  constructor(options) {
+    super(Object.assign(options, { sistinePermissionLevels }));
+    Object.defineProperty(this, 'keys', { value: keys });
+    this.datadog = dogstatsd;
+    this.queue = new Music();
+    this.interval = null;
+    this.banlist = bans;
+    this.raven = raven;
+  }
+}
+
+const Sistine = new SistineClient({
   clientOptions: {
     fetchAllMembers: false,
   },
