d041ccafb4c586bde020bc31dc23205bfdc698f1
Kashall <Kashalls@users.noreply.github.com>
2017-10-07 17:15:31 -0700

    ESlint fix manual

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const { Client, PermissionLevels } = require('klasa');
-const Music = require('./util/lib/Music');
-const keys = require('./keys.json');
-const bans = require('./banlist.json');
-const { StatsD } = require('node-dogstatsd');
-const Raven = require('raven');
+const { Client, PermissionLevels } = require('klasa')
+const Music = require('./util/lib/Music')
+const keys = require('./keys.json')
+const bans = require('./banlist.json')
+const { StatsD } = require('node-dogstatsd')
+const Raven = require('raven')
 
-const dogstatsd = new StatsD();
+const dogstatsd = new StatsD()
 
 const sistinePermissionLevels = new PermissionLevels()
   .addLevel(0, false, () => true)
@@ -13,18 +13,17 @@ const sistinePermissionLevels = new PermissionLevels()
   .addLevel(2, false, (client, msg) => msg.guild && msg.guild.settings.modRole && msg.member.roles.has(msg.guild.settings.modRole))
   .addLevel(7, false, (client, msg) => msg.guild && msg.member === msg.guild.owner)
   .addLevel(9, true, (client, msg) => msg.author === client.owner)
-  .addLevel(10, false, (client, msg) => msg.author === client.owner);
+  .addLevel(10, false, (client, msg) => msg.author === client.owner)
 
 class SistineClient extends Client {
+  constructor (options) {
+    super(Object.assign(options, { sistinePermissionLevels }))
+    Object.defineProperty(this, 'keys', { value: keys })
 
-  constructor(options) {
-    super(Object.assign(options, { sistinePermissionLevels }));
-    Object.defineProperty(this, 'keys', { value: keys });
-
-    this.datadog = dogstatsd;
-    this.queue = new Music();
-    this.banlist = bans;
-    this.raven = Raven;
+    this.datadog = dogstatsd
+    this.queue = new Music()
+    this.banlist = bans
+    this.raven = Raven
   }
 }
 
@@ -37,6 +36,6 @@ const Sistine = new SistineClient({
   cmdLogging: true,
   typing: false,
   permissionLevels: sistinePermissionLevels,
-});
+})
 
-Sistine.login(keys.botToken);
+Sistine.login(keys.dev ? keys.betaBotToken : keys.botToken)
