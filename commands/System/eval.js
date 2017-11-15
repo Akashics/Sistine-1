68231608f488a8f581c8d965315f938905ee1bf4
Kashall <Kashalls@users.noreply.github.com>
2017-11-14 19:46:49 -0800

    Update

--- commands/System/eval.js
+++ commands/System/eval.js
@@ -1,8 +1,9 @@
 const { Command, Stopwatch } = require('klasa');
 const { inspect } = require('util');
 const discord = require("discord.js"); //eslint-disable-line
+const { post } = require('snekfetch');
 
-module.exports = class extends Command {
+module.exports = class EvalCommand extends Command {
 
 	constructor(...args) {
 		super(...args, {
@@ -14,32 +15,36 @@ module.exports = class extends Command {
 	}
 
 	async run(msg, [...code]) {
+        const client = msg.client; // eslint-disable-line
+        const guild = msg.guild; // eslint-disable-line 
+        const settings = this.client.settings; // eslint-disable-line
 		const start = new Stopwatch();
 		try {
 			const evaled = eval(code.join(' '));
-			let kashEval = evaled;
-			if (evaled instanceof Promise) kashEval = await kashEval;
-			if (typeof evaled !== 'string') kashEval = inspect(kashEval, { depth: this.depth, showHidden: true });
-			const cleanEval = this.client.methods.util.clean(kashEval);
-			if (kashEval.length > 1950) {
-				const haste = await this.client.haste(cleanEval, 'js').catch(console.error);
-				msg.send(`**Took:** \`${start.stop()}\`, **Typeof:** \`${typeof evaled || evaled.constructor.name}\`
+			let ogeval = evaled;
+			if (evaled instanceof Promise) ogeval = await ogeval;
+			if (typeof evaled !== 'string') ogeval = inspect(ogeval, { depth: this.depth, showHidden: true });
+			const cleanEval = this.client.methods.util.clean(ogeval);
+			if (ogeval.length > 1950) {
+				const haste = await this.haste(cleanEval, 'js').catch(console.error);
+				msg.send(`**Took:** \`${start.stop()}\`, **Typeof:** \`${evaled.constructor.name || typeof evaled}\`
 \`Input:\`
 ${this.client.methods.util.codeBlock('js', code.join(' '))}
 \`Output:\` **Evaled code was over 2000 letters Here yo go **${haste}`).catch(console.error);
 			} else {
-				msg.send(`**Took:** \`${start.stop()}\`, **Typeof:** \`${typeof evaled || evaled.constructor.name}\`
-\`Input:\`\n${this.client.methods.util.codeBlock('js', code.join(' '))}
-\`Output:\` \`\`\`js
-${cleanEval}\`\`\`
+				msg.send(`**Took:** \`${start.stop()}\`, **Typeof:** \`${evaled.constructor.name || typeof evaled}\`
+\`Input:\`
+${this.client.methods.util.codeBlock('js', code.join(' '))}
+\`Output:\`
+${this.client.methods.util.codeBlock('js', cleanEval)}
 `).catch(console.error);
 			}
 		} catch (err) {
 			msg.send(`
-**Took:** \`${start.stop()}\n
+**Took:** \`${start.stop()}\`
 \`Input:\`
 ${this.client.methods.util.codeBlock('js', code.join(' '))}
-\`ERROR\`
+\`Error:\`
 ${this.client.methods.util.codeBlock('js', err)}`).catch(console.error);
 			if (err.stack) this.client.emit('error', err.stack);
 		}
@@ -47,6 +52,15 @@ ${this.client.methods.util.codeBlock('js', err)}`).catch(console.error);
 
 	async init() {
 		this.depth = 0;
+	}
+
+	haste(input, extension) {
+		return new Promise((res, rej) => {
+			if (!input) rej('Input argument is required.');
+			post('https://hastebin.com/documents').send(input).then(body => {
+				res(`https://hastebin.com/${body.body.key}${extension ? `.${extension}` : ''}`);
+			}).catch(e => rej(e));
+		});
 	}
 
 };
---
+++ events/guildMemberRemove.js
@@ -0,0 +1,76 @@
+const { Event } = require('klasa');
+const { Canvas } = require('canvas-constructor');
+const snek = require('snekfetch');
+const { resolve, join } = require('path');
+
+Canvas.registerFont(resolve(join(__dirname, '../assets/fonts/Discord.ttf')), 'Discord');
+
+module.exports = class guildMemberAdd extends Event {
+
+	constructor(...args) {
+		super(...args, { name: 'guildMemberRemove', enabled: true });
+		this.makeLeaveImg = async (person, guildname) => {
+			const png = person.displayAvatarURL.replace(/\.gif.+/g, '.png');
+			const { body } = await snek.get(png);
+			const pName = person.tag;
+			const WelName = 'GOODBYE';
+			const enjoyStay = `We hope you enjoyed here at ${guildname}`;
+			return new Canvas(1024, 450)
+				.addImage(body, 384, 20, 256, 256, { type: 'round', radius: 128 })
+				.restore()
+				.setTextFont('80px Discord')
+				.setColor('#FFFFFF')
+				.setTextAlign('center')
+				.addText(WelName, 512 - (WelName.length / 2), 350)
+				.setTextFont('38px Discord')
+				.addText(pName, 512 - (pName.length / 2), 388)
+				.setTextFont('30px Discord')
+				.addText(enjoyStay, 512 - (enjoyStay.length / 2), 440)
+				.toBuffer();
+		};
+	}
+
+	async run(member) {
+		this.client.stats.increment('client.memberLeaves');
+		if (!member.guild.settings.logChannel) { return; }
+		const byebyemsg = [
+			`**${member.user.tag}** is gone, Cries !!!`,
+			`**${member.user.tag}** ran away seeing a :snake: :stuck_out_tongue_winking_eye:`,
+			`We have lost our player **${member.user.tag}**!`,
+			`We need a substitute for **${member.user.tag}**.`,
+			`Please say goodbye to **${member.user.tag}** we will miss you!`,
+			`**${member.user.tag}** left without telling bye :neutral_face: .`,
+			`I had sensed something wiered about **${member.user.tag}**`
+		];
+		var randomNumber = Math.floor(Math.random() * byebyemsg.length);
+		const embed = new this.client.methods.Embed()
+			.setColor(0xE0082d)
+			.setDescription(byebyemsg[randomNumber])
+			.setThumbnail(member.user.displayAvatarURL())
+			.setTimestamp()
+			.setFooter(this.client.user.username, this.client.user.avatarURL());
+
+		// trying to send the log detail to log channel
+		const logChannelID = member.guild.settings.logChannel;
+		if (!logChannelID) { return; }
+		const logLevel = member.guild.settings.playerLogLevel;
+		try {
+			switch (logLevel) {
+				case 1:
+					member.guild.channels.get(logChannelID).send(byebyemsg[randomNumber]);
+					break;
+				case 2:
+					member.guild.channels.get(logChannelID).send({ embed });
+					break;
+				case 3:
+					member.guild.channels.get(logChannelID).send({ files: [{ attachment: await this.makeLeaveImg(member.user, member.guild.name), name: `Goodbye_${member.user.tag}.png` }] });
+					break;
+				default:
+					break;
+			}
+		} catch (error) {
+			this.client.console.error(error);
+		}
+	}
+
+};
