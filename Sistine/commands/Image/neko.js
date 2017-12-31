bd6dabc572e55273836fb1a3f81da1f1baf40cd1
Kashall <Kashalls@users.noreply.github.com>
2017-12-30 20:18:52 -0800

    Fixed

--- Sistine/commands/Image/neko.js
+++ Sistine/commands/Image/neko.js
@@ -1,20 +1,19 @@
 const { Command } = require('klasa');
-const snek = require("snekfetch");
+const snek = require('snekfetch');
 
 module.exports = class Neko extends Command {
 
-    constructor(...args) {
-        super(...args, {
-            cooldown: 10,
-            requiredSettings: [],
-            description: 'This command will return a Neko, a lewd Neko if used in a NSFW channel.'
-        });
-    }
+	constructor(...args) {
+		super(...args, {
+			cooldown: 10,
+			requiredSettings: [],
+			description: 'This command will return a Neko, a lewd Neko if used in a NSFW channel.'
+		});
+	}
 
-    async run(msg) {
-        const { body } = await snek.get(`https://nekos.life/api${msg.channel.nsfw === true ? "/lewd" : ""}/neko`);
-        return message.channel.send({ embed: { image: { url: body.neko } } });
-        
-    }
+	async run(msg) {
+		const { body } = await snek.get(`https://nekos.life/api${msg.channel.nsfw === true ? '/lewd' : ''}/neko`);
+		return msg.channel.send({ embed: { image: { url: body.neko } } });
+	}
 
 };
