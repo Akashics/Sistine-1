16f4026381719b8bafa18b33026af9e6a4c25cd2
Kashall <Kashalls@users.noreply.github.com>
2017-12-31 17:59:45 -0800

    updoot

--- Sistine/finalizers/balanceUpdating.js
+++ Sistine/finalizers/balanceUpdating.js
@@ -10,7 +10,6 @@ module.exports = class balanceUpdater extends Finalizer {
 		if (!msg.command.cost) return;
 		const newAmount = msg.author.configs.balance - msg.command.cost;
 		msg.author.configs.update('balance', newAmount, msg.guild);
-		return;
 	}
 
 };
