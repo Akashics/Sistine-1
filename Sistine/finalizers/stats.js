eb19bead048be14d1a7b73d617679b0fac9ddb2d
Kashall <Kashalls@users.noreply.github.com>
2018-01-15 19:01:01 -0800

    Fix

--- Sistine/finalizers/stats.js
+++ Sistine/finalizers/stats.js
@@ -9,10 +9,13 @@ module.exports = class CMDStats extends Finalizer {
 		});
 	}
 
-	run(msg, mes, stopwatch) {
+	async run(msg, mes, stopwatch) {
 		this.client.stats.histogram('command.process_time', stopwatch.end - stopwatch.start);
 		this.client.stats.increment(`cmd.${msg.command.name}`);
 		this.client.stats.increment('command.totalCommands');
+
+		const total = await this.client.providers.default.incrementValue('stats', '45cfe4d5-7901-4b3e-94ee-1c951a230f25', 'commands', 1);
+		this.client.providers.default.update('stats', '45cfe4d5-7901-4b3e-94ee-1c951a230f25', { commands: total });
 	}
 
 };
