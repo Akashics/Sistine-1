0f16d3dd59a6bb4bfabfea389ac82d2f1008c31a
Kashall <Kashalls@users.noreply.github.com>
2018-01-15 18:58:41 -0800

    fix

--- Sistine/lib/API.js
+++ Sistine/lib/API.js
@@ -4,7 +4,7 @@ const app = express();
 /* eslint-disable max-len */
 module.exports = async (client) => {
 	app.get('/api', async (req, res) => {
-		const rethink = await client.providers.default.db('test').table('stats').get('45cfe4d5-7901-4b3e-94ee-1c951a230f25wwwwwwww').run();
+		const rethink = await client.providers.default.db('test').table('stats').get('45cfe4d5-7901-4b3e-94ee-1c951a230f25').run();
 		const guilds = (await client.shard.fetchClientValues('guilds.size')).reduce((prev, val) => prev + val, 0);
 		const users = (await client.shard.fetchClientValues('users.size')).reduce((prev, val) => prev + val, 0);
 		const channels = (await client.shard.fetchClientValues('channels.size')).reduce((prev, val) => prev + val, 0);
