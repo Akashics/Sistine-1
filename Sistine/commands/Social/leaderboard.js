780c1f630dc8706589ee889784d6a29bd2d94e2a
Kashall <Kashalls@users.noreply.github.com>
2018-01-22 20:31:05 -0800

    Fix Leaderboard + Ready Event

--- Sistine/commands/Social/leaderboard.js
+++ Sistine/commands/Social/leaderboard.js
@@ -23,9 +23,9 @@ module.exports = class Leaderboards extends Command {
 		if (page > totalPages && !totalPages) return msg.channel.send(`There are only **${totalPages || 1}** pages in the leaderboard.`);
 		if (totalPages && page + 1 > totalPages) return msg.channel.send(`There are only **${totalPages || 1}** pages in the leaderboard.`);
 
-		leaderboard.push('= = = Global Leaderboard = = =\n\n');
+		leaderboard.push('= = = Global Leaderboard = = =\n');
 
-		const pos = leadboardPosition.indexOf(msg.author.id).toString().padStart(2, '0');
+		const pos = leadboardPosition.findIndex(i => i.id === msg.author.id).padStart(2, '0');
 
 		leadboardPosition.slice(page * 10, (page + 1) * 10)
 			.map(user => ({ balance: user.balance, user: user.id }))
@@ -33,7 +33,7 @@ module.exports = class Leaderboards extends Command {
 				leaderboard.push(` • ${((page * 10) + (position + 1)).toString().padStart(2, ' ')} | ${this.client.users.get(newMap.user).tag.padEnd(30, ' ')}::  ${this.client.users.get(newMap.user).configs.balance.toLocaleString()}`)
 			);
 
-		leaderboard.push(` • ${pos !== -1 ? pos + 1 : '???'} | ${msg.author.tag.padEnd(30, ' ')}::  ${this.client.users.get(msg.author.id).configs.balance.toLocaleString()}`);
+		leaderboard.push(`\n  • ${pos !== -1 ? pos + 1 : '???'} | ${msg.author.tag.padEnd(30, ' ')}::  ${this.client.users.get(msg.author.id).configs.balance.toLocaleString()}`);
 		leaderboard.push('--------------------------------------------------');
 		return msg.channel.send(`${leaderboard.join('\n')}\n Page ${page + 1} / ${totalPages || 1} - ${leadboardPosition.length} Total Users`, { code: 'asciidoc' });
 	}
