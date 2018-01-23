ae54302f218893dffea58eba5cdbdd8f21ad1c79
Kashall <Kashalls@users.noreply.github.com>
2018-01-22 20:31:05 -0800

    Fix Leaderboard + Ready Event

--- Sistine/events/ready.js
+++ Sistine/events/ready.js
@@ -11,14 +11,13 @@ module.exports = class Ready extends Event {
 		setInterval(() => {
 			Sistine.stats.gauge('client.users', Sistine.users.size);
 			Sistine.stats.gauge('client.ping', Sistine.ping);
-			Sistine.stats.gauge('client.memory', `${process.memoryUsage().heapUsed}`);
+			Sistine.stats.gauge('client.memory', process.memoryUsage().heapUsed);
 		}, 30000);
 
 		Sistine.emit('log', `[RAVEN] Sentry.io logging is ${Sistine.raven.installed ? 'enabled' : 'disabled'}.`);
-		updateStatus(Sistine);
-		webhook(`\`\`\`tex\n$ [READY] Shard[${shardid}]: Sistine ready! Guilds: ${Sistine.guilds.size.toLocaleString()}, Users: ${Sistine.users.size.toLocaleString()}\`\`\``);
-
 		if (shardid === 0) require('../lib/API')(this.client);
+		webhook(`\`\`\`tex\n$ [READY] Sistine Shard ${shardid} is available to ${Sistine.guilds.size.toLocaleString()} guilds.\`\`\``);
+		updateStatus(Sistine);
 	}
 
 };
