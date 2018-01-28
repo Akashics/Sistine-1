db96a8d20396bb297ec063d0da3241880fcf446a
Kashall <Kashalls@users.noreply.github.com>
2018-01-28 08:49:55 -0800

    Oops again

--- Sistine/tasks/reminder.js
+++ Sistine/tasks/reminder.js
@@ -1,9 +1,9 @@
-const { Task } = require('klasa');
+const { Task, Timestamp } = require('klasa');
 
 module.exports = class Reminder extends Task {
 
 	async run({ user, text, from }) {
-		return user.send(`<a:ditto:393547954168004608> ${user.username}, you asked me to remind you to \`${text}\` about ${from} from now.`);
+		return user.send(`<a:ditto:393547954168004608> ${user.username}, you asked me to remind you to \`${text}\` about ${Timestamp.toNow(from)} from now.`);
 	}
 
 };
