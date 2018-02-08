f3b9cdcd09f1fe3e8f40a506ecb5da5f586c5315
Kashall <Kashalls@users.noreply.github.com>
2018-02-07 21:45:40 -0800

    darn

--- Dashboard/Dashboard.js
+++ Dashboard/Dashboard.js
@@ -46,7 +46,7 @@ app.get('/woah', (req, res) => {
 
 app.get('/commands', async (req, res) => {
 	const { body } = await snekfetch.get("https://api.sistine.ml/commands"); //.catch(() => renderTemplate(res, req, 'index.ejs'));
-	const commands = body;
+	const commands = body.data;
 	renderTemplate(res, req, 'commands.ejs', commands);
 });
 
