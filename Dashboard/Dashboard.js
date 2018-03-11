b41ee10ce7637b1d9f0c418f4ce3fdca4c951652
Kashall <Kashalls@users.noreply.github.com>
2018-03-10 19:27:42 -0800

    Misspelling get off my lawn

--- Dashboard/Dashboard.js
+++ Dashboard/Dashboard.js
@@ -46,8 +46,8 @@ app.get('/woah', (req, res) => {
 
 app.get('/commands', async (req, res) => {
 	const { body } = await snekfetch.get("https://api.sistine.ml/commands"); //.catch(() => renderTemplate(res, req, 'index.ejs'));
-	const result = JSON.parse(body);
-	return renderTemplate(res, req, 'commands.ejs', { result });
+	const commands = JSON.parse(body);
+	return renderTemplate(res, req, 'commands.ejs', { commands });
 });
 
 app.get("/stats", async (req, res) => {
