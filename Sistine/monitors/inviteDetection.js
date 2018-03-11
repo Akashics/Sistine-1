ba32b1422010e4f1958f9b73a71d73207c116ccf
Kashall <Kashalls@users.noreply.github.com>
2018-03-10 19:25:38 -0800

    Damn Invite Detection

--- Sistine/monitors/inviteDetection.js
+++ Sistine/monitors/inviteDetection.js
@@ -21,8 +21,8 @@ module.exports = class extends Monitor {
 	}
 
 	async init() {
-		if (!this.client.gateways.guilds.schema.hasKey('antiinvite')) {
-			await this.client.gateways.guilds.schema.addKey('antiinvite', { type: 'boolean', default: false });
+		if (!this.client.gateways.guilds.schema.has('antiinvite')) {
+			await this.client.gateways.guilds.schema.add('antiinvite', { type: 'boolean', default: false });
 		}
 	}
 
