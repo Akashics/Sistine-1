b786147fbcd782d4e1389de97bb65a041a5fa7dc
Kashall <Kashalls@users.noreply.github.com>
2018-04-18 20:29:21 -0700

    Push fix for subscribe

--- commands/Informational/subscribe.js
+++ commands/Informational/subscribe.js
@@ -15,7 +15,7 @@ module.exports = class extends Command {
 
 	async run(msg) {
 		const role = announcement(msg);
-		await msg.member.addRole(role);
+		await msg.member.roles.add(role, 'Subscribed to Announcements');
 		return msg.send(msg.language.get('COMMAND_SUBSCRIBE_SUCCESS', role.name));
 	}
 
