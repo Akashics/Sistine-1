4ea9497c48c411145ced7ba9d5f62d706c8af78b
Kashall <jordpjones@gmail.com>
2018-05-18 21:58:39 -0700

    Who needs this, when you got infinite classes

--- src/lib/util/webhooks.js
+++
@@ -1,8 +0,0 @@
-const config = require('../../config.json');
-
-const { WebhookClient } = require('discord.js');
-const hooker = new WebhookClient(config.webhook.id, config.webhook.token);
-
-module.exports = (msg) => {
-	hooker.send(msg, { disableEveryone: true });
-};
